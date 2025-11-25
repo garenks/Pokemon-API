fe3c01dd1bda76fd68f4d9e7abd1e0a5343dbc44
Paulo <paulovitor.vitinho30@gmail.com>
2025-11-25 16:54:15 -0300

    Feat: functionality to pull Pokemon data via API

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const container = document.getElementById("cards-container");
+
+// Buscar lista de Pokémon
+fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(pokemon => {
+
+            // Puxado dos detalhes de cada Pokémon
+            fetch(pokemon.url)
+                .then(response => response.json())
+                .then(details => {
+                    
+                    // Criação do card do Pokémon da API
+                    const card = document.createElement("div");
+                    card.classList.add("card");
+
+                    const img = document.createElement("img");
+                    img.src = details.sprites.front_default;
+
+                    const name = document.createElement("h3");
+                    name.innerText = details.name.toUpperCase();
+
+                    const type = document.createElement("p");
+                    type.innerText = "Tipo: " + details.types[0].type.name;
+
+                    card.appendChild(img);
+                    card.appendChild(name);
+                    card.appendChild(type);
+
+                    container.appendChild(card);
+
+                });
+        });
+    });
